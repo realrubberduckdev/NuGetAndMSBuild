f3d38c6c698b2ca70c7c520a902efd17fdb7bac1
Dushyant Priyadarshee <dushyant.priyadarshee@sharp-ls.eu>
2020-05-10 16:27:59 +0100

    Add code for talk.

---
+++ Consumer/Consumer/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Reflection;
+
+namespace Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
