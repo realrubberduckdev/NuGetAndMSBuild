6fa8ec5c7ef029992c82106d059a0b5f60c84dae
Dushyant Priyadarshee <dushyant.priyadarshee@sharp-ls.eu>
2020-05-11 15:29:29 +0100

    Add ppt and InitializeTalk.ps1

---
+++ InitializeTalk.ps1
@@ -0,0 +1,9 @@
+nuget.exe locals all -clear
+Start-Process "https://www.meetup.com/dotnetoxford/events/270269847/"
+Start-Process "https://marketplace.visualstudio.com/items?itemName=rubberduckdev.SDKStyleTemplatesNet"
+Start-Process "https://docs.microsoft.com/en-us/nuget/create-packages/creating-a-package#from-a-convention-based-working-directory"
+Start-Process "https://docs.microsoft.com/en-us/nuget/reference/msbuild-targets#packing-using-a-nuspec"
+Start-Process "https://github.com/yck1509/ConfuserEx"
+Start-Process "https://www.nuget.org/packages/ConfuserEx.Final/"
+Start-Process "NuGet & MSBuild.pptx"
+code .
